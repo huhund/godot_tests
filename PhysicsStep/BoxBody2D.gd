d8027255d93ea1ceef0164edc00dccfb0a70b7cb
h <>
2020-05-01 13:57:15 +0300

    added phsyics step testing

---
+++ PhysicsStep/BoxBody2D.gd
@@ -0,0 +1,24 @@
+extends RigidBody2D
+
+var physicsFrame = 0
+var renderFrame = 0
+
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(deltaTime):
+	physicsFrame += 1
+	print("\n_physics_process: " + str(physicsFrame)+ " msec " + str(OS.get_ticks_msec()))
+
+	if physicsFrame >= 10:
+		set_linear_velocity(Vector2(10,0))
+
+	force_update_transform()
+	print("\tpos " + str(transform.origin))
+	print("\tvel " + str(get_linear_velocity()))
+	
+func _process(deltaTime):
+	if renderFrame != physicsFrame:
+		print("_process: " + str(physicsFrame) + " msec " + str(OS.get_ticks_msec()))
+
+	renderFrame	= physicsFrame
---
+++ PhysicsStep/BoxBody2DIntegrate.gd
@@ -0,0 +1,30 @@
+extends RigidBody2D
+
+var physicsFrame = 0
+var renderFrame = 0
+
+func _ready():
+	pass # Replace with function body.
+
+func _integrate_forces(bodyState):
+	print("_integrate_forces: " + str(physicsFrame) + " msec " + str(OS.get_ticks_msec()))
+#	if physicsFrame >= 10:
+#		bodyState.linear_velocity = Vector2(10,0)
+	if physicsFrame == 10:
+		bodyState.apply_central_impulse(Vector2(10,0))
+		
+	print("\tvel " + str(bodyState.linear_velocity))
+
+
+func _physics_process(deltaTime):
+	physicsFrame += 1
+	print("\n_physics_process: " + str(physicsFrame)+ " msec " + str(OS.get_ticks_msec()))
+
+	force_update_transform()
+	print("\tpos " + str(transform.origin))
+	
+func _process(deltaTime):
+	if renderFrame != physicsFrame:
+		print("_process: " + str(physicsFrame) + " msec " + str(OS.get_ticks_msec()))
+
+	renderFrame	= physicsFrame
